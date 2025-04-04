e066e89b6f841d2a0995d3b5d37e66250d862dbc
Pablo Rodríguez Pérez <43758706+pablorodriper@users.noreply.github.com>
2025-04-04 23:34:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
@@ -6,6 +7,11 @@ app = FastAPI()
 def read_root():
     return {"message": "Welcome to my Render-hosted API!"}
 
+@app.get("/music/")
+def read_root():
+    redirect_url = "https://music.apple.com/es/album/in-dream/1016488205"
+    return RedirectResponse(redirect_url)
+    
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "query": q}
