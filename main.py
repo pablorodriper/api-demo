32567b662039da9798d84b6388ffa92cd449f84f
Pablo Rodríguez Pérez <43758706+pablorodriper@users.noreply.github.com>
2025-04-05 00:07:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,25 @@ app = FastAPI()
 def read_root():
     return {"message": "Welcome to my Render-hosted API!"}
 
-@app.get("/music/", response_class=RedirectResponse, status_code=302)
-async def get_music_page():
+@app.get("/music", response_class=RedirectResponse, status_code=302)
+async def get_music():
     redirect_url = "https://music.apple.com/es/album/in-dream/1016488205"
     return redirect_url
-    
+
+@app.get("/music_iframe", response_class=HTMLResponse)
+def get_music_iframe():
+    html_content = """
+    <html>
+      <head>
+        <title>Random Music</title>
+      </head>
+      <body>
+        <iframe allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" frameborder="0" height="450" style="width:100%;max-width:660px;overflow:hidden;border-radius:10px;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/es/album/in-dream/1016488205"></iframe>
+      </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content)
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "query": q}
