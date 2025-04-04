8acb0af581d7f584d2272e1d261ea35cb6632d62
Pablo Rodríguez Pérez <43758706+pablorodriper@users.noreply.github.com>
2025-04-04 23:52:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,22 +11,8 @@ def read_root():
 
 @app.get("/music/")
 def get_music_page():
-    redirect_url = "https://music.apple.com/es/album/in-dream/1016488205"
+    redirect_url = "music.apple.com/es/album/in-dream/1016488205"
     return RedirectResponse(redirect_url)
-
-@app.get("/music_iframe/", response_class=HTMLResponse)
-def get_music_iframe():
-    html_content = """
-    <html>
-      <head>
-        <title>Your Random Disc</title>
-      </head>
-      <body>
-        <iframe allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" frameborder="0" height="450" style="width:100%;max-width:660px;overflow:hidden;border-radius:10px;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/es/album/in-dream/1016488205"></iframe>
-      </body>
-    </html>
-    """
-    return HTMLResponse(content=html_content)
     
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
